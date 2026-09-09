bba37dcf0994a6071ad3bf296eb009fac3209a69
Norbert Fila <nubet2137@gmail.com>
2026-09-09 16:26:26 +0200

    fix: use proper job author selector

--- src/infrastructure/useme_selectors.py
+++ src/infrastructure/useme_selectors.py
@@ -2,6 +2,8 @@ JOB_ARTICLE_SELECTOR = "article.job"
 JOB_CONTENT_SELECTOR = "div.job__content"
 NEXT_PAGE_SELECTOR = 'a[rel="next"]'
 AUTHOR_CANDIDATE_SELECTORS = (
+    ".job__headline strong",
+    ".job__headline b",
     ".job__header-details strong",
     ".job__header strong",
     ".job__header-details b",
